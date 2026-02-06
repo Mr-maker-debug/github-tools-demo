741e608f0805bc077cd2952d7ba52edd03ee4ff5
Mr-maker-debug <66715526+Mr-maker-debug@users.noreply.github.com>
2026-02-06 17:25:01 +0800

    在 develop 分支添加 HelloWorld 组件

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>这是 GitHub 工具演示项目</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+.hello {
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
